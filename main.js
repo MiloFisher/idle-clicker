da4e814b6f9b3c419f460ab301cc6a390ee60f71
MiloFisher <tbfmilo@gmail.com>
2021-11-02 17:26:47 -0700

    Set up mvp code outline

--- main.js
+++ main.js
@@ -1,22 +1,108 @@
+// UI Objects
+let gameBackground;         // Game Background sprite
+let clickArea;              // Main button for clicking to get Ants
+let workerAntsDisplay;      // Text showing worker ants amount
+let sugarGrainsDisplay;     // Text showing sugar grains amount
+let lowerBackground;        // Allocation/Upgrades background sprite
+let upgradesTabButton;      // Upgrades tab button
+let allocationTabButton;    // Ant allocation tab button
+let upgradesTabElements;    // Array holding upgrades tab UI elements
+let allocationTabElements;  // Array holding allocation tab UI elements
+let upgradesTabBackground;  // Upgrades tab background
+let allocationTabBackground;// Allocation tab background
 
-var button, button1, sprite, text, text1, spriteSheet, animation;
+// Game Objects
+let workerAnts;             // Amount of worker ants the player has
+let sugarGrains;            // Amount of sugar grains the player has
+let workerAntRate;          // Worker ant rate in Sugar Grains per second
 
+
+// Called at start of game, before update()
 function start() {
-    console.log("start");
-    button = new Button("rhino.jpeg", 30, 30, 60, 45, 0, startAnim);
-    button1 = new Button("rhino.jpeg", 30, 90, 60, 45, 1, stopAnim);
-    sprite = new Sprite("rhino.jpeg", 0, 0, 720, 360, 2);
-    text = new RenderText("Let's Go!", GAME.WIDTH / 2, 40, "40px Arial", "red", "center", false, -1);
-    text = new RenderText("LLLLLLLLLL", GAME.WIDTH / 2 + 50, 40, "60px Arial", "black", "center", false, 1);
-    spriteSheet = new SpriteSheet("testSpriteSheet.png", 2, 4);
-    animation = new RenderAnimation(spriteSheet, 200, 150, 60, 60, 12, true, -2);
-}
+    initializeUIElements();
+    initializeGameObjects();
 
+    showUpgradesTab(); // Start out with upgrades tab open
+}
+let lastVal = 0;
+// Called every game tick, 60 ticks in a second
 function update() {
-    if(GAME.TICKS % 60 == 0) {
-        console.log("One Second");
-    }
+    gainSugarGrains();
 }
 
-function stopAnim() { animation.stop(); }
-function startAnim() { animation.play(); }
+// Initializes all UI elements
+function initializeUIElements() {
+    gameBackground = new Sprite("gray.png", 0,0, GAME.WIDTH, GAME.HEIGHT, 100);
+    clickArea = new Button("white.png", 0, 0, GAME.WIDTH, GAME.HEIGHT * 0.5, -100, gainWorkerAnts, [1]);
+    clickArea.sprite.visible = false; // Make click area a invisible
+    workerAntsDisplay = new RenderText("Worker Ants: 0", 10, 20, "20px Comic Sans", "black", "left", false, 0);
+    sugarGrainsDisplay = new RenderText("Sugar Grains: 0", GAME.WIDTH - 10, 20, "20px Comic Sans", "black", "right", false, 0);
+    lowerBackground = new Sprite("black.png", 0, GAME.HEIGHT * 0.5, GAME.WIDTH, GAME.HEIGHT * 0.5, 90);
+    upgradesTabButton = new Button("upgradesTabButton.png", 150, GAME.HEIGHT * 0.5 + 20, 100, 25, 0, showUpgradesTab);
+    allocationTabButton = new Button("allocationTabButton.png", GAME.WIDTH - 250, GAME.HEIGHT * 0.5 + 20, 100, 25, 0, showAllocationTab);
+    upgradesTabElements = [];
+    allocationTabElements = [];
+    upgradesTabElements.push(upgradesTabBackground = new Sprite("white.png", 50, GAME.HEIGHT * 0.5 + 50, GAME.WIDTH - 100, GAME.HEIGHT * 0.5 - 100, 80));
+    allocationTabElements.push(allocationTabBackground = new Sprite("gray.png", 50, GAME.HEIGHT * 0.5 + 50, GAME.WIDTH - 100, GAME.HEIGHT * 0.5 - 100, 80));
+}
+
+// Initializes all Game Objects
+function initializeGameObjects() {
+    workerAnts = 0;         // Starting ants amount
+    sugarGrains = 0;        // Starting sugar grains amount 
+    workerAntRate = 0.5;    // Starting worker ant sugar collection rate
+}
+
+// Use this function to add to the amount of worker ants
+function gainWorkerAnts(amount) {
+    workerAnts += amount;
+    // Later we can add big number formatting here to simplify as like "5.5B"
+    workerAntsDisplay.text = "Worker Ants: " + workerAnts;
+}
+
+// Gains sugar gains based on how many worker ants there are
+function gainSugarGrains() {
+    sugarGrains += workerAntRate * workerAnts / 60;
+    // Later we can add big number formatting here to simplify as like "5.5B"
+    sugarGrainsDisplay.text = "Sugar Grains: " + ~~sugarGrains;
+}
+
+// Shows all upgrade tab elements, hiding all allocation tab elements
+function showUpgradesTab() {
+    upgradesTabElements.forEach(e => {
+        if(e instanceof Button) {
+            e.sprite.visible = true;
+        }
+        else {
+            e.visible = true;
+        }
+    });
+    allocationTabElements.forEach(e => {
+        if (e instanceof Button) {
+            e.sprite.visible = false;
+        }
+        else {
+            e.visible = false;
+        }
+    });
+}
+
+// Shows all allocation tab elements, hiding all upgrades tab elements
+function showAllocationTab() {
+    allocationTabElements.forEach(e => {
+        if (e instanceof Button) {
+            e.sprite.visible = true;
+        }
+        else {
+            e.visible = true;
+        }
+    });
+    upgradesTabElements.forEach(e => {
+        if (e instanceof Button) {
+            e.sprite.visible = false;
+        }
+        else {
+            e.visible = false;
+        }
+    });
+}
